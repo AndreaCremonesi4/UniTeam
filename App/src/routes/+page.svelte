d338bd41fa571467ed1a4d0e39a5168bee30cb21
m.nigro2 <m.nigro2@studenti.unibg.it>
2023-09-27 15:28:16 +0200

    Update +page.svelte

--- App/src/routes/+page.svelte
+++ App/src/routes/+page.svelte
@@ -1,7 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <h1>Welcome to UniTeam Mondo!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<h2>modified by Nigro Marco</h2>
+<button>Press me!</button>
