580bb021880928cdc26df3b30a73122775e89caf
m.nigro2 <m.nigro2@studenti.unibg.it>
2023-09-27 15:17:51 +0200

    Update +page.svelte

--- App/src/routes/+page.svelte
+++ App/src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<<<<<<< Updated upstream
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+=======
+<h1>Welcome to UniTeam Mondo!</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h2>questa è la modifica di Nigro Marco</h2>
+>>>>>>> Stashed changes
