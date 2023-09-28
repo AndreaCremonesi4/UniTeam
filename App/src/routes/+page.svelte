6ff6abed1bdfa872abaa856737994baf64295749
AndreaCremonesi4 <114904033+AndreaCremonesi4@users.noreply.github.com>
2023-09-28 16:40:23 +0200

    Prima modifica Home Page

--- App/src/routes/+page.svelte
+++ App/src/routes/+page.svelte
@@ -1,3 +1,35 @@
-<h1>Welcome to UniTeam Mondo!</h1>
+<script>
+	import Navbar from '../components/navbar/Navbar.svelte';
+</script>
 
-<button>Press me!</button>
+<Navbar />
+
+<section class="container-sm" style="margin-top: 8rem;">
+	<div class="hero">
+		<div class="hero-body">
+			<h1 class="text-gradient-reverse main-header">La Tua Community Universitaria Online</h1>
+			<p class="text-body">
+				Recensisci Corsi e Professori, Collabora in Gruppi di Studio, e Comunica in Tempo Reale con
+				Altri Studenti Universitari
+			</p>
+			<button
+				class="btn btn-primary btt-gradient-reverse sub-header mt-2"
+				style="padding: 1.15rem 3rem; box-shadow: 2px 5px 10px 0px rgba(28, 53, 93, 0.30);">Unisciti Ora!</button
+			>
+		</div>
+		
+		<img class="hero-img" src="assets/images/hero.png" alt="" />
+	</div>
+
+	<div class="row justify-content-center align-items-center sub-header my-5">
+		<div class="col text-center">
+            <p class="text-title mb-3 text-center sub-header">Scopri di più</p>
+			<img src="assets/icons/arrow-down.svg" style="height:40px;" alt="" />
+		</div>
+	</div>
+</section>
+
+<section class="container">
+	<h2 class="fw-bolder text-center display-6 text-dark">I Nostri Servizi</h2>
+	
+</section>
