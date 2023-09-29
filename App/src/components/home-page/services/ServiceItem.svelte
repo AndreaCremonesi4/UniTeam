4fe2ed6e092ce6f17c97153d31c4c3f7e016be84
Andrea Cremonesi <114904033+AndreaCremonesi4@users.noreply.github.com>
2023-09-29 13:01:25 +0200

    Updated Home Page

---
+++ App/src/components/home-page/services/ServiceItem.svelte
@@ -0,0 +1,41 @@
+<script>
+	export let src, text;
+</script>
+
+<div class="service">
+	<div class="service-image gradient-light">
+		<img {src} alt="" />
+	</div>
+	<div class="service-text">
+		<p>{text}</p>
+	</div>
+</div>
+
+<style>
+	.service {
+		display: flex;
+		flex-direction: column;
+		max-width: min(250px, 90%);
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		gap: 1rem;
+	}
+
+	.service-image {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 110px;
+		aspect-ratio: 1;
+		border-radius: 10000px;
+	}
+
+	.service-image > img {
+		width: min(50px, 50vw);
+	}
+
+	.service-text {
+		margin-bottom: 1rem;
+	}
+</style>
